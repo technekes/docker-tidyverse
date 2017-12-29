e72730569f7de9b2af5130a4ec3dcd0e17b3cac7
Jack A Ross <jack.ross@technekes.com>
2017-12-29 14:15:53 -0500

    Initial Commit

---
+++ sampleSQL.r
@@ -0,0 +1,8 @@
+library(DBI)
+library(odbc)
+dbConn<-dbConnect(odbc(),
+  driver="{ODBC Driver 13 for SQL Server}",
+  server="mhknbn2kdz.database.windows.net",
+  database="AdventureWorks2012",
+  uid="sqlfamily",
+  pwd="sqlf@m1ly")
